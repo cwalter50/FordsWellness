77cb61051a8b6c638a545e8162ce9adae830d4cf
cwalter50 <walt50@gmail.com>
2025-04-03 13:22:47 -0400

    Initial Commit

---
+++ FordsWellness/FordsWellnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  FordsWellnessApp.swift
+//  FordsWellness
+//
+//  Created by Christopher Walter on 4/3/25.
+//
+
+import SwiftUI
+
+@main
+struct FordsWellnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
