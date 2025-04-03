ab969e0c0c3483ac7a97d9456bdf9ecb33e55552
cwalter50 <walt50@gmail.com>
2025-04-03 14:17:52 -0400

    working on CreateActivitesView

---
+++ FordsWellness/Activity.swift
@@ -0,0 +1,8 @@
+//
+//  Activity.swift
+//  FordsWellness
+//
+//  Created by Christopher Walter on 4/3/25.
+//
+
+import Foundation
---
+++ FordsWellness/CreateActivityView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateActivityView.swift
+//  FordsWellness
+//
+//  Created by Christopher Walter on 4/3/25.
+//
+
+import SwiftUI
+
+struct CreateActivityView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateActivityView()
+}
